70967161ee5b957d3dc58bfd06317f8eb26ccbb0
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-07-21 19:32:34 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Dhruv Bhardwaj on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
