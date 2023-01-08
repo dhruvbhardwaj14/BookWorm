252c7309b34b1754c7211e6917429299c3bb6423
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2023-01-08 16:22:41 +0530

    Completed BookWorm Project

--- BookWorm/BookWormApp.swift
+++ BookWorm/BookWormApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct BookWormApp: App {
+    @StateObject private var dataController=Datacontroller()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -6,13 +6,70 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @Environment(\.managedObjectContext) var moc
+    //for sorting the books-using two sortdescriptor sorts data with first property then by author
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
+
+    @State private var showingAddScreen = false
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let book = books[offset]
+
+            // delete it from the context
+            moc.delete(book)
+        }
+
+        // save the context-comment this line to not delete when the app runs again for that session
+        try? moc.save()
     }
-}
+    
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(books){ book in
+                    NavigationLink{
+                       DetailView(book: book)
+                    }label: {
+                        HStack{
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            VStack(alignment: .leading){
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                    .foregroundColor((book.rating==1) ? .red : .primary)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteBooks)
+            }
+           .navigationTitle("Bookworm")
+           .toolbar {
+               ToolbarItem(placement: .navigationBarLeading) {
+                   EditButton()
+               }
+               ToolbarItem(placement: .navigationBarTrailing) {
+                   Button {
+                       showingAddScreen.toggle()
+                   } label: {
+                       Label("Add Book", systemImage: "plus")
+                   }
+               }
+           }
+           .sheet(isPresented: $showingAddScreen) {
+               AddBookView()
+           }
+       }
+    }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
